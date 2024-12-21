d7170e70879c1e3b1dd50a710a12856a77212fa9
Alexander Anufriev <anufriev.alexander@gmail.com>
2024-12-21 22:12:57 +0300

    добавлена функция удаления текста из строки

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
+#define FILE_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "file.h"
+
+
+char *str_remove(char *str, char *sub) {
+    size_t len = strlen(sub);
+    if (len > 0) {
+        char *p = str;
+        while ((p = strstr(p, sub)) != NULL) {
+            memmove(p, p + len, strlen(p + len) + 1);
+        }
+    }
+    return str;
+}
+
 int main() {
     char *filename = "example.txt";
 
